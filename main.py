68dc6899c6e18ea60845dd4147fb059a6edcf6d4
mgirish97 <meghanagirish@hotmail.com>
2021-09-10 22:45:23 -0700

    Change sqlite to postgres

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ from typing import Callable
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from sqlalchemy.exc import IntegrityError
 from functools import wraps
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -29,7 +30,7 @@ gravatar = Gravatar(
 )
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
